b5d80a16f2f73b236af2fa425cbdd652a01ebe7b
OnlyForAshutosh20799 <onlyashu20799@gmail.com>
2024-11-04 17:55:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Text from './components/Text'
+function App() {
+  
+
+  return (
+    <>
+      <Text/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Text.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { FaBold, FaItalic, FaUnderline } from "react-icons/fa";
+
+function Text() {
+  const [text, setText] = useState("");
+  const [boldStyle,setBoldStyle]= useState(false)
+  const [italicStyle, setItalicStyle]= useState(false)
+  const [underlineStyle, setUnderline]= useState(false)
+  const [capitalized, setCapitalized]=useState(false)
+
+
+  const boldText=()=>{
+    setBoldStyle(!boldStyle)   
+  }
+
+  const italicText=()=>{
+        setItalicStyle(!italicStyle)
+  }
+
+  const underline=()=>{
+        setUnderline(!underlineStyle)
+  }
+
+  const uppercasetext=()=>{
+        let newtext=text.toUpperCase()
+        setText(newtext)       
+  }
+
+  const lowercasetext=()=>{
+    let newtext=text.toLowerCase()
+    setText(newtext)
+  }
+
+  const capitalizeHandle=()=>{
+    setCapitalized(!capitalized)
+  }
+
+  const copyText = () => {
+    navigator.clipboard.writeText(text).then(() => {
+      alert("Copied");
+    }).catch((error) => {
+      console.error("Failed to copy text:", error);
+    });
+  };
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+
+  const clear=()=>{
+    setText("");
+    setBoldStyle(false);
+    setItalicStyle(false);
+    setUnderline(false);
+    setupperCaseStyle(false);
+    setCapitalized(false);
+  }
+
+
+  const textStyle ={
+    fontWeight:boldStyle ? "bold":'normal',
+    fontStyle:italicStyle ? "italic": 'normal',
+    textDecoration:underlineStyle ? "underline" : 'none',
+    textTransform:capitalized ?"capitalize":'none'
+  }
+
+
+
+  return (
+    <>
+      <section className="pt-8">
+        <h1 className="text-4xl font-bold text-center pt-4">Text Analyzer</h1>
+        <div className="mx-2 md:mx-40 lg:mx-60">
+          <p className="font-bold text-2xl">Enter Your Text Here</p>
+          <textarea
+            onChange={handleTextChange}
+            style={textStyle}
+            value={text}
+            placeholder="Enter your text"
+            className="border-4 w-full h-60 mt-4 rounded-lg border-gray-600 pt-2 pl-2"
+          ></textarea>
+
+          <div className="mx-auto space-x-2 my-2 grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-9 gap-2">
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center"
+            onClick={boldText}
+            >
+              <FaBold />
+            </button>
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center"
+            onClick={italicText}
+            >
+              <FaItalic />
+            </button>
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center"
+            onClick={underline}
+            >
+              <FaUnderline />
+            </button>
+
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center"
+            onClick={uppercasetext}
+            >
+              ABC
+            </button>
+
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center"
+            onClick={lowercasetext}
+            >
+              abc
+            </button>
+        
+
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center"
+            onClick={capitalizeHandle}
+            >
+              Title Case
+            </button>
+            {/* <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center">
+              Trim
+            </button> */}
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center"
+              onClick={copyText}
+            >
+              Copy Text
+            </button>
+            <button className="bg-gray-600 text-white rounded-md p-2 flex justify-center items-center" 
+            onClick={clear}
+            >
+              Clear Text
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Text;
